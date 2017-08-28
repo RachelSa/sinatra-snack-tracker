306ff04b42808f1402924acd02bf406e7bb782f6
Rachel Salois <rachel.salois@gmail.com>
2017-08-28 16:20:46 -0400

    lecture code

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,8 +1,13 @@
 class SnacksController < ApplicationController
-
+  # index
   get '/snacks' do
     @snacks = Snack.all
     erb :'/snacks/index'
+  end
+
+  get '/snacks/:id' do
+     @snack = Snack.find(params[:id])
+     erb :'/snacks/show'
   end
 
 
